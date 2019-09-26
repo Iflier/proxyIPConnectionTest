9ef8f84f0173a0eb722348f21c78ab3a70b378be
Iflier <alexalf@163.com>
2019-09-27 00:00:32 +0800

    Remove: remove waitgroup from genListElem func

--- connectionTest.go
+++ connectionTest.go
@@ -13,25 +13,21 @@ import (
 	"time"
 
 	"github.com/go-redis/redis"
+
+	"BasicTool"
 )
 
+const MAXGOROUTINES int = 10
+
+// 用于解析反序列化后的 json 字符串
 type ProxyIP struct {
 	HttpType string
 	Ip       string
 }
 
-var redisOptions = redis.Options{
-	Network:    "tcp",
-	Addr:       "localhost:6379",
-	DB:         0,
-	Password:   "******",
-	MaxRetries: 5,
-}
-
-func genListElem(ch chan string, wg *sync.WaitGroup, ipList *list.List) {
+func genListElem(ch chan string, ipList *list.List) {
 	// 通过 chan 向各个 go routines 传递列表元素
 	var next *list.Element
-	wg.Add(1)
 	if ipList.Len() > 0 {
 		for elem := ipList.Front(); elem != nil; elem = next {
 			ch <- elem.Value.(string)
@@ -39,11 +35,11 @@ func genListElem(ch chan string, wg *sync.WaitGroup, ipList *list.List) {
 			ipList.Remove(elem)
 		}
 	}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < MAXGOROUTINES; i++ {
 		ch <- ""
+		// 等待一定时间，让各个 goroutines 依次退出
 		time.Sleep(10 * time.Millisecond)
 	}
-	wg.Done()
 }
 
 func testAvailableProxyIP(proxyStr string) bool {
@@ -87,7 +83,7 @@ func main() {
 	// http 和 https 类型的代理。也代表着redis中的两种集合
 	var validHttpType = [...]string{"HTTP", "HTTPS"}
 	var structInstance *ProxyIP = new(ProxyIP)
-	var cache = redis.NewClient(&redisOptions)
+	var cache = redis.NewClient(&RedisDBTool.RedisOptions)
 	var file, err = os.Open("proxy.json")
 	defer file.Close()
 	if err != nil {
@@ -107,15 +103,15 @@ func main() {
 		intCmdPointer = cache.SCard(httpType)
 		// 仅当集合元素个数大于 0 时
 		if intCmdPointer.Val() > 0 {
-			fmt.Printf("%v elements in %v\n", intCmdPointer.Val(), httpType)
+			fmt.Printf("%v elements in %v set.\n", intCmdPointer.Val(), httpType)
 			stringSlicePointer = cache.SMembers(httpType)
 			for _, value := range stringSlicePointer.Val() {
 				elemList.PushBack(value)
 			}
 		}
 	}
-	go genListElem(elemChan, waitGroup, elemList)
-	for i := 0; i < 10; i++ {
+	go genListElem(elemChan, elemList)
+	for i := 0; i < MAXGOROUTINES; i++ {
 		fmt.Printf("i = %v\n", i)
 		waitGroup.Add(1)
 		go addProxyIP(elemChan, waitGroup, cache)
